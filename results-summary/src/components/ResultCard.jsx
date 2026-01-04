50c8841f4320803765d01e7f88f31719903a5317
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-04 04:54:06 +0100

    feat: Improvements to ResultCard styling

--- results-summary/src/components/ResultCard.jsx
+++ results-summary/src/components/ResultCard.jsx
@@ -1,14 +1,16 @@
 function ResultCard({ category, score, icon }) {
-  <div className="w-full rounded-md px-4 py-6 items-center justify-between">
-    <div>
-      <img src={icon} alt={`${category} icon`} />
-      <h4>{category}</h4>
+  return (
+    <div className="w-full rounded-md px-4 py-6 flex items-center justify-between">
+      <div>
+        <img src={icon} alt={`${category} icon`} />
+        <h4>{category}</h4>
+      </div>
+      <div>
+        <span className="text-dark-grey-blue mr-2">{score}</span>
+        <p className="inline-flex">/ 100</p>
+      </div>
     </div>
-    <div>
-      <span>{score}</span>
-      <p>/ 100</p>
-    </div>
-  </div>;
+  );
 }
 
 export default ResultCard;
