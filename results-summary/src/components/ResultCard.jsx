525713136c58919ac1e50d80b11cc0d168ec09c1
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-04 04:27:47 +0100

    feat: Script for components

--- results-summary/src/components/ResultCard.jsx
+++ results-summary/src/components/ResultCard.jsx
@@ -1,5 +1,14 @@
 function ResultCard({ category, score, icon }) {
-    
+  <div className="w-full rounded-md px-4 py-6 items-center justify-between">
+    <div>
+      <img src={icon} alt={`${category} icon`} />
+      <h4>{category}</h4>
+    </div>
+    <div>
+      <span>{score}</span>
+      <p>/ 100</p>
+    </div>
+  </div>;
 }
 
-export default ResultCard
+export default ResultCard;
--- results-summary/src/components/common/Button.jsx
+++ results-summary/src/components/common/Button.jsx
@@ -1,5 +1,9 @@
 function Button({ text }) {
-  return <button>{text}</button>;
+  return (
+    <button className="w-full rounded-full bg-dark-grey-blue text-white text-center p-5 cursor-pointer hover:bg-linear-b hover:from-light-slate-blue hover:to-light-royal-blue">
+      {text}
+    </button>
+  );
 }
 
 export default Button;
