43f4e99d0fedf9ec89d7a0a515ddcb9e97eead4b
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-08 08:54:24 +0100

    fix(icons): Import results category icons in utils js not data.json

---
+++ results-summary/src/utils/categoryTheme.js
@@ -0,0 +1,26 @@
+import reactionIcon from "../assets/images/icon-reaction.svg";
+import memoryIcon from "../assets/images/icon-memory.svg";
+import verbalIcon from "../assets/images/icon-verbal.svg";
+import visualIcon from "../assets/images/icon-visual.svg";
+
+export default categoryTheme = {
+  Reaction: {
+    classes: "bg-light-red/5 text-light-red",
+    icon: reactionIcon,
+  },
+
+  Memory: {
+    classes: "bg-orangey-yellow/5 text-orangey-yellow",
+    icon: memoryIcon,
+  },
+
+  Verbal: {
+    classes: "bg-green-teal/5 text-green-teal",
+    icon: verbalIcon,
+  },
+
+  Visual: {
+    classes: "bg-cobalt-blue/5 text-cobalt-blue",
+    icon: visualIcon,
+  },
+};
