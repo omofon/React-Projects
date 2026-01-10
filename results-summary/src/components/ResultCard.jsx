d144e3927ea07eaca33fc5a019409ff1dd7ad8d6
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-10 05:25:28 +0100

    feat(tailwindcss): Refine Results Summary styling and button hover states for high-fidelity alignment with the design.

--- results-summary/src/components/ResultCard.jsx
+++ results-summary/src/components/ResultCard.jsx
@@ -4,15 +4,15 @@ function ResultCard({ category, score, icon }) {
   const theme = categoryStyles[category];
   return (
     <div
-      className={`w-full rounded-lg px-4 py-4 flex items-center justify-between ${theme}`}
+      className={`w-full rounded-lg p-4 flex items-center justify-between lg:min-w-2xs ${theme}`}
     >
-      <div className="flex gap-2 items-center">
-        <img src={icon} alt={`${category} icon`} />
-        <h4 className="font-medium">{category}</h4>
+      <div className="flex gap-3 items-center">
+        <img src={icon} alt={`${category} icon`} className="w-5 h-5" />
+        <h4 className="font-bold text-lg">{category}</h4>
       </div>
-      <div>
-        <span className="text-dark-grey-blue mr-2">{score}</span>
-        <p className="inline-flex">/ 100</p>
+      <div className="flex items-center">
+        <span className="text-dark-grey-blue mr-2 font-bold">{score}</span>
+        <span className="text-light-lavendar ml-2">/ 100</span>
       </div>
     </div>
   );
--- results-summary/src/components/common/Button.jsx
+++ results-summary/src/components/common/Button.jsx
@@ -6,7 +6,7 @@ function Button({ text }) {
   return (
     <button
       onClick={alertButton}
-      className="w-full rounded-full bg-dark-grey-blue text-white text-center text-xl font-bold p-5 cursor-pointer hover:bg-linear-to-b hover:from-light-slate-blue hover:to-light-royal-blue transition-all duration-500"
+      className="w-full rounded-full bg-dark-grey-blue text-white text-center text-xl font-bold py-4 cursor-pointer hover:bg-linear-to-b hover:from-light-slate-blue hover:to-light-royal-blue transition-colors duration-300"
     >
       {text}
     </button>
